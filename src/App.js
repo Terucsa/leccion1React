8a4e7e42b389c41ecd3705dc4826a5a5a7ee4e4b
beltervas <98856514+Terucsa@users.noreply.github.com>
2023-03-04 16:21:14 -0600

    Primer programa de react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './css/estilos.css'
 
-function App() {
+const App = () => {
+
+    const saludo = "React - INTECAP"
+    const nombre ="Belter Emanuel Vásquez Flores"
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+        <h1 className='titulo'>{saludo}</h1>
+        <p className='titulo'>{nombre}</p>
+    </>
+
+  )
 }
 
-export default App;
+export default App
